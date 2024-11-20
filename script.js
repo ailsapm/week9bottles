b4368c44335c18a244ae4acc2a2bed56ffbad618
ailsapm <alimooney2013@gmail.com>
2024-11-20 16:29:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//it was displaying fine in console.log but cannot get it to print out correctly in allocated div on html
+
+function showlyrics (){
+
+for (var numBtls = 99; numBtls >= 0; numBtls--) {
+    if (numBtls >= 3) {
+        document.getElementById("reveal").innerHTML+= (numBtls + " bottles of beer on the wall,<br>");
+        document.getElementById("reveal").innerHTML+= (numBtls + " bottles of beer,<br>");
+        document.getElementById("reveal").innerHTML+= ("Take one down, pass it around,<br>");
+        document.getElementById("reveal").innerHTML+= ((numBtls-1) + " bottles of beer.<br>");
+   }
+   else if (numBtls == 2) {
+    document.getElementById("reveal").innerHTML+= (numBtls + " bottles of beer on the wall,<br>");
+    document.getElementById("reveal").innerHTML+= (numBtls + " bottles of beer,<br>");
+    document.getElementById("reveal").innerHTML+= ("Take one down, pass it around,<br>");
+    document.getElementById("reveal").innerHTML+= ((numBtls-1) + " bottle of beer.<br>");
+}
+   else if (numBtls == 1 ) {
+    document.getElementById("reveal").innerHTML += (numBtls + " bottle of beer on the wall,<br>");
+    document.getElementById("reveal").innerHTML += (numBtls + " bottle of beer,<br>");
+    document.getElementById("reveal").innerHTML += ("Take one down, pass it around,<br>");
+    document.getElementById("reveal").innerHTML += ("No more bottles of beer.<br>");
+}
+   else if (numBtls <= 0) {
+    document.getElementById("reveal").innerHTML += ("No more bottles of beer on the wall,<br>"); 
+    document.getElementById("reveal").innerHTML += ("No more bottles of beer,<br>");
+    document.getElementById("reveal").innerHTML += ("Go to the store and buy some more,<br>");
+    document.getElementById("reveal").innerHTML += ("99 bottles of beer.");
+   }
+}
+}
